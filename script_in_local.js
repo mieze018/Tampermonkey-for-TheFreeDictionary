05bb601a14a31cc4ed340f3a1a6bc38fd05f7e3d
Ayu Nakata <mieze018@users.noreply.github.com>
2022-07-18 15:34:05 +0900

    deepL

--- script_in_local.js
+++ script_in_local.js
@@ -8,19 +8,24 @@
   $('aside#sidebar').append(`<section class="widget"><iframe src="https://www.bing.com/images/search?q=${searchedWord}&amp;FORM=HDRSC2" style=" height: 500px;"></section>`);
 
   const commonPopupFeatures = 'menubar=no,toolbar=no,location=no,scrollbars=no,status=no,width=250,height=500';
+  const languageTargetTranslation = 'ja'
 
   //flaticon
   const openFlaticonPopupWindow = window.open(`https://www.flaticon.com/search?word=${searchedWord}`, "flaticon", `${commonPopupFeatures}`);
   openFlaticonPopupWindow.focus();
 
-
-  const openIconFinderPopupWindow = window.open(`https://www.iconfinder.com/search/?q=${searchedWord}`, "icon8", `${commonPopupFeatures} top=500 `);
+  // icon finder
+  const openIconFinderPopupWindow = window.open(`https://www.iconfinder.com/search/?q=${searchedWord}`, "icon8", `${commonPopupFeatures} top=580 `);
   openIconFinderPopupWindow.focus();
+
+  //deepL
+  const openDeepPopupWindow = window.open(`https://www.deepl.com/translator#en/${languageTargetTranslation}/${searchedWord}`, "deepL", `${commonPopupFeatures} top=1160 left=0 width=500 `);
 
   //on leave page
   window.onbeforeunload = () => {
     openFlaticonPopupWindow?.close();
     openIconFinderPopupWindow?.close();
+    openDeepPopupWindow?.close();
   }
 
 })();
