2a1c2f502ec8d7249f26cc5bf28933641df9bc05
Ayu Nakata <mieze018@users.noreply.github.com>
2022-07-18 14:44:20 +0900

    const openIconFinderPopupWindow

--- script_in_local.js
+++ script_in_local.js
@@ -7,9 +7,14 @@
   //bing images
   $('aside#sidebar').append(`<section class="widget"><iframe src="https://www.bing.com/images/search?q=${searchedWord}&amp;FORM=HDRSC2" style=" height: 500px;"></section>`);
 
-  const commonPopupFeatures = 'menubar=no,toolbar=no,location=no,scrollbars=no,status=no,width=200,height=500';
+  const commonPopupFeatures = 'menubar=no,toolbar=no,location=no,scrollbars=no,status=no,width=250,height=500';
 
   //flaticon
   const openFlaticonPopupWindow =()=> window.open(`https://www.flaticon.com/search?word=${searchedWord}`, "flaticon", `${commonPopupFeatures}`);
   openFlaticonPopupWindow()
+
+  const openIconFinderPopupWindow = () => window.open(`https://www.iconfinder.com/search/?q=${searchedWord}`, "icon8", `${commonPopupFeatures} top=500 `);
+  openIconFinderPopupWindow()
+
+
 })();
