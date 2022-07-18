8eb0096dd600530e1ddb34d278fdc3b27ccde371
Ayu Nakata <mieze018@users.noreply.github.com>
2022-07-18 14:31:26 +0900

    open flaticon popup window

--- script_in_local.js
+++ script_in_local.js
@@ -4,6 +4,10 @@
 (function () {
   'use strict';
   const searchedWord = location.href.split('/')[3];
+  //bing images
   $('aside#sidebar').append(`<section class="widget"><iframe src="https://www.bing.com/images/search?q=${searchedWord}&amp;FORM=HDRSC2" style=" height: 500px;"></section>`);
 
+  //flaticon
+  const openFlaticonPopupWindow =()=> window.open(`https://www.flaticon.com/search?word=${searchedWord}`, "flaticon", 'height=500,width=500,toolbar=0,location=0,menubar=0"');
+  openFlaticonPopupWindow()
 })();
