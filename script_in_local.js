dd87d509923391c3dc73b7aa33c658d4c3b569fb
Ayu Nakata <mieze018@users.noreply.github.com>
2022-07-18 16:22:36 +0900

    update deepL search

--- script_in_local.js
+++ script_in_local.js
@@ -3,7 +3,9 @@
 // ==/UserScript==
 (function () {
   'use strict';
-  const searchedWord = location.href.split('/')[3];
+  const undecodedSearchedWord = location.href.split('/')[3];
+  //url query decode
+  const searchedWord = decodeURIComponent(undecodedSearchedWord).replace(/\+/g, ' ');
 
   const commonPopupFeatures = 'menubar=no,toolbar=no,location=no,scrollbars=no,status=no,width=500,height=500';
   const languageSource = 'en'
@@ -14,7 +16,20 @@
   openIconFinderPopupWindow.focus();
 
   //deepL
-  const openDeepPopupWindow = window.open(`https://www.deepl.com/translator#en/${languageTargetTranslation}/${searchedWord}`, "deepL", `${commonPopupFeatures} top=1160 left=0 width=500 `);
+  const newLineForDeepL = '%0A'
+  const definitionElements1 = document.getElementsByClassName('ds-single');
+  const definitionElements2 = document.getElementsByClassName('ds-list');
+  const definitionText1 = Array.from(definitionElements1).map(element => element.innerText).join(newLineForDeepL).replace(/\n/g, newLineForDeepL);
+  console.log(definitionText1);
+  const definitionText2 = Array.from(definitionElements2).map(element => element.innerText).join(newLineForDeepL);
+  // console.log(definitionText);
+  const openDeepPopupWindow = window.open(`https://www.deepl.com/translator#en/${languageTargetTranslation}/${searchedWord}
+  ${newLineForDeepL}${newLineForDeepL}
+  ${definitionText1}
+  ${newLineForDeepL}${newLineForDeepL}
+  ${definitionText2}
+  `, "deepL", `${commonPopupFeatures} top=1160 left=0 width=500 `);
+
 
   //on leave page
   window.onbeforeunload = () => {
