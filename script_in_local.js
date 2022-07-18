417ad00687a9403406c0f2c70dfcbcf9496ab964
Ayu Nakata <mieze018@users.noreply.github.com>
2022-07-18 16:10:07 +0900

    remove bing widget

--- script_in_local.js
+++ script_in_local.js
@@ -4,8 +4,6 @@
 (function () {
   'use strict';
   const searchedWord = location.href.split('/')[3];
-  //bing images
-  $('aside#sidebar').append(`<section class="widget"><iframe src="https://www.bing.com/images/search?q=${searchedWord}&amp;FORM=HDRSC2" style=" height: 500px;"></section>`);
 
   const commonPopupFeatures = 'menubar=no,toolbar=no,location=no,scrollbars=no,status=no,width=500,height=500';
   const languageSource = 'en'
